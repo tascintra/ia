5df216aa14020fa2c15e786f971fbd72b3d9d448
Tiago Cintra <tiago.slv@hotmail.com>
2023-09-12 15:44:38 -0300

    feat: main header done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { Button } from './components/ui/button'
+import { Github } from "lucide-react"
+import { Button } from "./components/ui/button"
+import { Separator } from "./components/ui/separator"
 
 export function App() {
-
   return (
     <div>
-      <div>
-        <h1>upload.ai</h1>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
 
-        <div>
-          <span>Desenvolvido com 💜 no NLW da Rocketseat</span>
-          <Button>Github</Button>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" /> GitHub
+          </Button>
         </div>
       </div>
     </div>
