9ab590722beca307500a3704861d6721d37f96f4
Tiago Cintra <tiago.slv@hotmail.com>
2023-09-12 15:33:16 -0300

    feat: shadcn and tailwind installed and configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { Button } from './components/ui/button'
+
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <div>
+        <h1>upload.ai</h1>
+
+        <div>
+          <span>Desenvolvido com 💜 no NLW da Rocketseat</span>
+          <Button>Github</Button>
+        </div>
+      </div>
+    </div>
   )
 }
