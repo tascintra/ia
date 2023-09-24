382aede128d878425a1a0abba7c0dd888ba36491
Tiago Cintra <tiago.slv@hotmail.com>
2023-09-24 16:44:26 -0300

    feat: prompt select component created

--- upload-ai-web/src/app.tsx
+++ upload-ai-web/src/app.tsx
@@ -12,6 +12,7 @@ import {
 } from "./components/ui/select"
 import { Slider } from "./components/ui/slider"
 import { VideoInputForm } from "./components/video-input-form"
+import { PromptSelect } from './components/prompt-select'
 
 export function App() {
   return (
@@ -62,17 +63,7 @@ export function App() {
           <form className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">
-                    Descrição do YouTube
-                  </SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect />
             </div>
 
             <div className="space-y-2">
---
+++ upload-ai-web/src/components/prompt-select.tsx
@@ -0,0 +1,21 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select"
+
+export function PromptSelect() {
+  return (
+    <Select>
+      <SelectTrigger>
+        <SelectValue placeholder="Selecione um prompt..." />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="title">Título do YouTube</SelectItem>
+        <SelectItem value="description">Descrição do YouTube</SelectItem>
+      </SelectContent>
+    </Select>
+  )
+}
